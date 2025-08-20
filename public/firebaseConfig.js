b67c05671f5d6711f83969b27fd2805e4ad41aaa
Aninakwa <123469627+Aninakwa@users.noreply.github.com>
2025-08-20 23:20:22 +0000

    Update firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -9,13 +9,7 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyARvmrHc4h7VVX3qSikpFwQoCakIe6gTMc",
-  authDomain: "timerapp-b7c71.firebaseapp.com",
-  projectId: "timerapp-b7c71",
-  storageBucket: "timerapp-b7c71.firebasestorage.app",
-  messagingSenderId: "771458730610",
-  appId: "1:771458730610:web:3c5f00a127e7bdcb0ed0eb",
-  measurementId: "G-JSLBDE0T3L",
+,
 };
 
 // Initialize Firebase
